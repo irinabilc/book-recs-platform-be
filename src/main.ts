3e4a681c27749d4532eec27ecaa53e0370ab89ab
irinabilc <irinabilc@gmail.com>
2026-01-13 11:49:24 +0200

    feat: add prisma schema and initial app bootstrap with swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+
+if (process.env.NODE_ENV || process.env.NODE_ENV === 'prod') {
+    require('module-alias/register')
+}
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+    const APP_PORT = process.env.APP_PORT || 3000
+
+    const app = await NestFactory.create(AppModule, { cors: true })
+    app.setGlobalPrefix('api')
+
+    const config = new DocumentBuilder()
+        .setTitle('NestJS Book Recommendations API')
+        .setDescription(
+            'NestJS Book Recommendations API done as a project to learn NestJS and Prisma'
+        )
+        .setVersion('0.1')
+        .build()
+    const document = SwaggerModule.createDocument(app, config)
+    SwaggerModule.setup('doc', app, document)
+
+    await app.listen(APP_PORT, () => {
+        console.log(`Application is running on: http://localhost:${APP_PORT}`)
+    })
 }
-bootstrap();
+bootstrap()
