1e1773521d648d268e0efbe7bc97cd7b9d34334e
irinabilc <irinabilc@gmail.com>
2026-01-25 21:08:07 +0200

    added users and books resources to nest app and updated prisma schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { UsersModule } from './users/users.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { BooksModule } from './books/books.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [UsersModule, PrismaModule, BooksModule],
+    controllers: [AppController],
+    providers: [AppService],
 })
 export class AppModule {}
